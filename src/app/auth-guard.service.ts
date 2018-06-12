fa24ad51b4753b5a96be558a6b11978c0f88a7de
Yamit Villamil <yamit.villamil@globant.com>
2018-06-11 22:54:39 -0500

    Redirecting after logging

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 import 'rxjs/add/operator/map';
 
@@ -8,14 +8,21 @@ export class AuthGuardService implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     // This method can return 3 values: Obervable<boolean> | Promise<boolean> | boolean
     // Instead of subscribe we are going to call map operator that return an observable.
     // Angular internally subscribe into this new boolean observable and then remove this subscription later.
     return this.auth.user$.map(user => {
       if (user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(
+        ['/login'], 
+        {
+          queryParams: { 
+            returnUrl: state.url 
+          }
+        });
+        
       return false;
     });
   }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,17 +2,33 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>; // Make an observable from firebase.User
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
     this.user$ = afAuth.authState; // Unwrap this Observable in our template using async pipe, this pipe automatically unsubscribe from this observable when the component is destroy
   }
 
   login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+
+    localStorage.setItem('returnUrl', returnUrl);
+
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(auth => {
+        if (auth.user) {
+          const returnUrl = localStorage.getItem('returnUrl');
+
+          this.router.navigateByUrl(returnUrl);
+        }
+      });
   }
 
   logout() {
